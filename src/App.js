10ad82ac82ea26c5f372e50948a53a27f5a2891c
s.mousavi <smuosavi.360@gmail.com>
2021-07-07 10:15:57 +0300

    main

--- src/App.js
+++ src/App.js
@@ -2,18 +2,7 @@ import axios from 'axios';
 import './App.css'
 import React, {useEffect,useState} from 'react';
 import { useHistory } from "react-router-dom";
-// import './styles.css';
 import  data from './data';
-const products=
-[
-  { id: 1, name: 'Cheese', price: 4.9, stock: 20 },
-  { id: 2, name: 'Milk', price: 1.9, stock: 32 },
-  { id: 3, name: 'Yoghurt', price: 2.4, stock: 12 },
-  { id: 4, name: 'Heavy Cream', price: 3.9, stock: 9 },
-  { id: 5, name: 'Butter', price: 0.9, stock: 99 },
-  { id: 6, name: 'Sour Cream ', price: 2.9, stock: 86 },
-  { id: 7, name: 'Fancy French Cheese 🇫🇷', price: 99, stock: 12 }
-]
 
 const useSortableData = (items, config = null) => {
   const [sortConfig, setSortConfig] = React.useState(config);
